ba06e47b7f37d82d6f5b4ddaaaf752b856f2a44b
dhruv <dhruv.160410116084@gmail.com>
2020-04-14 15:31:45 +0530

    templete added

---
+++ codechef-april-long-2020/___.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.Integer.*;
+
+
+public class ___ {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+}
